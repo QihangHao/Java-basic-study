6344540d3eab0bc62437091aa2dea85a29e6533e
Hao Qihang <h_qihang@163.com>
2026-03-15 20:22:44 +0800

    feat(LeetCode001): 实现两数之和交互式解决方案
    - 添加Scanner和HashMap导入语句
    - 实现控制台输入功能，支持用户输入目标值
    - 添加数组元素输入处理，支持空格分隔的数字输入
    - 使用哈希表优化查找算法，时间复杂度O(n)
    - 实现实时输出匹配结果的索引对
    - 添加输入验证和边界条件处理

--- src/com/leetcode/LeetCode001.java
+++ src/com/leetcode/LeetCode001.java
@@ -1,10 +1,29 @@
 package com.leetcode;
 
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
 /**
  * @author 郝启航
  */
 public class LeetCode001 {
     static void main() {
-
+        Scanner sc = new Scanner(System.in);
+        Map<Integer, Integer> map = new HashMap<>();
+        System.out.println("请输入目标值：");
+        int target = sc.nextInt();
+        System.out.println("请输入数组数值用空格间隔：");
+        int i = 0;
+        while(sc.hasNextInt()){
+            int currentnum = sc.nextInt();
+            if(map.containsKey(target - currentnum)){
+                System.out.println("[" + map.get(target - currentnum) + "," + i + "]");
+                break;
+            }else{
+                map.put(currentnum, i);
+                i++;
+            }
+        }
     }
 }
