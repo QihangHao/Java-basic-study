0e7ec893b84e9a80ec74ef96f97858e0c5639fa9
Hao Qihang <h_qihang@163.com>
2026-03-18 14:41:10 +0800

    feat: 添加红包生成器功能
    - 实现红包金额随机分配算法
    - 支持指定总金额和人数生成红包
    - 确保每个红包最小金额为0.01元
    - 添加控制台输入输出界面
    - 实现金额精度控制和四舍五入处理

---
+++ src/com/daily/study/RedPacketGenerator.java
@@ -0,0 +1,35 @@
+package com.daily.study;
+
+import java.util.Scanner;
+
+/**
+ * @author 郝启航
+ */
+public class RedPacketGenerator {
+    public static void redPacketGenerator(double money, int people) {
+
+        double min = 0.01;
+        double redMoney = 0;
+        for (int i = 0; i < people; i++){
+            if(i == people - 1){
+                redMoney = (double)Math.round(money*100)/100;
+            }
+            else{
+                double max =money - (people - 1-i) * 0.01;
+                redMoney = (Math.random() * (max - min) + min);
+                redMoney = (double)Math.round(redMoney * 100) / 100;
+                money = money - redMoney;
+
+            }
+            System.out.println("第" + (i + 1) + "个人抢到的红包为：" + redMoney);
+        }
+    }
+
+    static void main() {
+        System.out.println("请输入红包金额和总共人数");
+        Scanner sc = new Scanner(System.in);
+        double money = sc.nextInt();
+        int people = sc.nextInt();
+        redPacketGenerator(money, people);
+    }
+}
