ae2b481031e272c7d5b1392739f9ea0071d093a0
Hao Qihang <h_qihang@163.com>
2026-03-23 11:20:56 +0800

    feat(oop): 添加学生类实现基本面向对象功能
    - 创建Student1类包含姓名和年龄属性
    - 实现构造函数、getter和setter方法
    - 添加eat、sleep和study行为方法
    - 创建Students1主类进行测试演示
    - 实现对象创建、方法调用和属性修改功能

---
+++ src/com/oop/Students1.java
@@ -0,0 +1,56 @@
+package com.oop;
+
+/**
+ * @author 郝启航
+ */
+class Student1 {
+    private String name;
+    private int age;
+
+    public Student1() {
+    }
+
+    public Student1(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void eat() {
+        System.out.println(name + "吃吃吃");
+    }
+
+    public void sleep() {
+        System.out.println(name + "睡睡睡");
+    }
+
+    public void study() {
+        System.out.println(name + "学习学习学习");
+    }
+}
+
+public class Students1 {
+    static void main() {
+        Student1  student =new Student1("人",18);
+        student.eat();
+        student.sleep();
+        student.study();
+        student.setName("狗");
+        System.out.println(student.getName());
+    }
+}
